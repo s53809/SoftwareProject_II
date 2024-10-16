f39535bb31881889d2e3f5694cfe39e670b3d7cf
DESKTOP-S84HTQO\Minseo Kim <dg4res@gmail.com>
2024-10-16 10:03:50 +0900

    10/16 : 7주 2강 코드 업로드 완료

--- IDE_src/13_example_1/13_example_1.ino
+++ IDE_src/13_example_1/13_example_1.ino
@@ -1,9 +1,20 @@
 #include <Servo.h>
 
 // Arduino pin assignment
+#define PIN_TRIG  12  // sonar sensor TRIGGER
+#define PIN_ECHO  13  // sonar sensor ECHO
 #define PIN_SERVO 10
 
-// configurable parameters
+// configurable parameters for sonar
+#define SND_VEL 346.0     // sound velocity at 24 celsius degree (unit: m/sec)
+#define PULSE_DURATION 10 // ultra-sound Pulse Duration (unit: usec)
+#define _DIST_MIN 180.0   // minimum distance to be measured (unit: mm)
+#define _DIST_MAX 360.0   // maximum distance to be measured (unit: mm)
+
+#define TIMEOUT ((INTERVAL / 2) * 1000.0) // maximum echo waiting time (unit: usec)
+#define SCALE (0.001 * 0.5 * SND_VEL) // coefficent to convert duration to distance
+
+// configurable parameters for Servo
 #define _DUTY_MIN 550 // servo full clock-wise position (0 degree)
 #define _DUTY_NEU 1500 // servo neutral position (90 degree)
 #define _DUTY_MAX 2400 // servo full counter-clockwise position (180 degree)
@@ -11,7 +22,9 @@
 #define _POS_START (_DUTY_MIN + 100)    // servo start position
 #define _POS_END   (_DUTY_MAX - 100)    // servo end position
 
-#define _SERVO_SPEED 30 // servo speed limit (unit: degree/second)
+#define _SERVO_SPEED 30 // servo angular speed (unit: degree/sec)
+
+// Loop Interval
 #define INTERVAL 20     // servo update interval (unit: msec)
 
 // global variables
@@ -27,22 +40,28 @@ int toggle_interval, toggle_interval_cnt;
 
 void setup() {
   // initialize GPIO pins
+  pinMode(PIN_TRIG, OUTPUT);  // sonar TRIGGER
+  pinMode(PIN_ECHO, INPUT);   // sonar ECHO
+  digitalWrite(PIN_TRIG, LOW);  // turn-off Sonar 
+
   myservo.attach(PIN_SERVO); 
   
   duty_target = duty_curr = _POS_START;
   myservo.writeMicroseconds(duty_curr);
   
   // initialize serial port
-  Serial.begin(57600);
+  Serial.begin(2000000);  // <----- baud rate
 
-  // convert angular speed into duty change per interval.
-  // Next two lines are WRONG. you have to modify.
+  // convert angular velocity into duty change per interval.
+  // duty_change_per_interval = 
+  //  (_DUTY_MAX - _DUTY_MIN) * (_SERVO_SPEED / 180) * (INTERVAL / 1000);
   duty_change_per_interval = 
-    (_DUTY_MAX - _DUTY_MIN) * (_SERVO_SPEED / 180.0) * (INTERVAL / 1000.0);
- 
-  // remove 'while(1) { }' lines after modify 
+    (float)(_DUTY_MAX - _DUTY_MIN) * (_SERVO_SPEED / 180.0) * (INTERVAL / 1000.0);
+  
+  // remove next three lines after finding answers
   Serial.print("duty_change_per_interval:");
   Serial.println(duty_change_per_interval);
+  //  while (1) { }
 
   // initialize variables for servo update.
   toggle_interval = (180.0 / _SERVO_SPEED) * 1000 / INTERVAL;
@@ -53,9 +72,13 @@ void setup() {
 }
 
 void loop() {
+  float  dist_raw;
+
   // wait until next sampling time. 
   if (millis() < (last_sampling_time + INTERVAL))
     return;
+
+  dist_raw = USS_measure(PIN_TRIG, PIN_ECHO); // read distance
 
   // adjust duty_curr toward duty_target by duty_change_per_interval
   if (duty_target > duty_curr) {
@@ -75,6 +98,8 @@ void loop() {
   Serial.print("Min:1000");
   Serial.print(",duty_target:"); Serial.print(duty_target);
   Serial.print(",duty_curr:");   Serial.print(duty_curr);
+  Serial.print(",dist_raw:");   Serial.print(dist_raw);
+  Serial.print(",duty_change_per_interval:");   Serial.print(duty_change_per_interval);
   Serial.println(",Max:2000");
 
   // toggle duty_target between _DUTY_MIN and _DUTY_MAX.
@@ -90,4 +115,14 @@ void loop() {
 
   // update last sampling time
   last_sampling_time += INTERVAL;
+}
+
+// get a distance reading from USS. return value is in millimeter.
+float USS_measure(int TRIG, int ECHO)
+{
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(PULSE_DURATION);
+  digitalWrite(TRIG, LOW);
+  
+  return pulseIn(ECHO, HIGH, TIMEOUT) * SCALE; // unit: mm
 }
