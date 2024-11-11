6f2697e0959d3d23969518fa42ca294e452958a0
DESKTOP-S84HTQO\Minseo Kim <dg4res@gmail.com>
2024-11-11 10:00:43 +0900

    11/11: 적외선 센서 거리 관측 코드

---
+++ IDE_src/ServoTest/ServoTest/ServoTest.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+
+// Arduino pin assignment
+#define PIN_SERVO         10
+
+#define _DUTY_MIN 550  // servo full clock-wise position (0 degree)
+#define _DUTY_NEU 1000 // servo neutral position (90 degree)
+#define _DUTY_MAX 2400 // servo full counter-clockwise position (180 degree)
+
+Servo myservo;
+
+void setup()
+{
+  myservo.attach(PIN_SERVO); 
+  myservo.writeMicroseconds(_DUTY_NEU);
+  
+  Serial.begin(57600);
+}
+
+void loop(){
+  
+}
