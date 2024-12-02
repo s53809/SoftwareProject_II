3efea17f8ebb94752970e5cc1b3280c55c50edd1
DESKTOP-S84HTQO\Minseo Kim <dg4res@gmail.com>
2024-12-02 10:09:25 +0900

    12/02 : D제어, PD제어 코드

--- IDE_src/ServoTest/ServoTest/ServoTest.ino
+++ IDE_src/ServoTest/ServoTest/ServoTest.ino
@@ -1,10 +1,19 @@
 #include <Servo.h>
 
 // Arduino pin assignment
-#define PIN_SERVO         10
+#define PIN_SERVO 10
+#define PIN_IR    A0
+
 #define _DUTY_MIN 550  // servo full clock-wise position (0 degree)
 #define _DUTY_NEU 1600 // servo neutral position (90 degree)
 #define _DUTY_MAX 2400 // servo full counter-clockwise position (180 degree)
+
+#define _INTERVAL_DIST    20 // distance sensor interval (unit: ms)
+#define _INTERVAL_SERIAL  20 // serial interval (unit: ms)
+
+#define _EMA_ALPHA 0.2    // EMA weight of new sample (range: 0 to 1)
+
+
 
 Servo myservo;
 
